7762edc5dfb5a64566008c2dd53bf78512ccae8b
TheNoobDevs <thenoobdevs2024@gmail.com>
2024-07-10 10:29:15 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# android_device_samsung_a35xjvxx
-work in progress | A356E
+# Android device tree for samsung SM-A356E (a35x)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ twrp_a35x.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from a35x device
+$(call inherit-product, device/samsung/a35x/device.mk)
+
+PRODUCT_DEVICE := a35x
+PRODUCT_NAME := omni_a35x
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A356E
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a35xjvxx-user 13 TP1A.220624.014 A356EXXS2AXEC release-keys"
+
+BUILD_FINGERPRINT := samsung/a35xjvxx/a35x:13/TP1A.220624.014/A356EXXS2AXEC:user/release-keys
