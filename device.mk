5a2c0ad45efcda30eafaaf3d0e33857b597de1c2
Jamie Falconer <mrfluffyoven@gmail.com>
2024-07-10 13:38:25 +0100

    add packages & flags

--- device.mk
+++ device.mk
@@ -6,3 +6,26 @@
 #
 
 LOCAL_PATH := device/samsung/a35x
+
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd 
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload \
+    checkpoint_gc
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Shipping level
+PRODUCT_SHIPPING_API_LEVEL := 32
+PRODUCT_TARGET_VNDK_VERSION := 32
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
